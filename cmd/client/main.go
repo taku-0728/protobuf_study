1e3773e8a9bc6ba1be1ed81543ad4725703ba992
taku-0728 <taku8270@gmail.com>
2024-07-17 22:06:48 +0900

    [add] streaming用の処理を追加

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	hellopb "mygrpc/pkg/grpc"
 	"os"
@@ -37,7 +39,8 @@ func main() {
 
 	for {
 		fmt.Println("1: send Request")
-		fmt.Println("2: exit")
+		fmt.Println("2: HelloServerStream")
+		fmt.Println("3: exit")
 		fmt.Print("please enter >")
 
 		scanner.Scan()
@@ -47,6 +50,8 @@ func main() {
 		case "1":
 			Hello()
 		case "2":
+			HelloServerStream()
+		case "3":
 			fmt.Println("bye.")
 			goto M
 		}
@@ -67,5 +72,33 @@ func Hello() {
 		fmt.Println(err)
 	} else {
 		fmt.Println(res.GetMessage())
+	}
+}
+
+func HelloServerStream() {
+	fmt.Println("Please enter your name.")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+	}
+	stream, err := client.HelloServerStream(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		res, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("all the responses hav already receive.")
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(res)
 	}
 }
